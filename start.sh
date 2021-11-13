07243341333f586d8c6ec72c8bc953e8d8f019e0
Elizaveta <elizabeth.prosvirina@rockscie.com>
2021-11-13 12:34:48 +0300

    checkpoint 2

--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-wget https://www.transfer.sh/VFqcVw/elastic.gzip --no-check-certificate
-tar -zcvfx elastic.gzip
-cd /elastic && ./bin/elasticsearch -d -p 100000
-
-cd / && python /app/service.py
+python service.py
